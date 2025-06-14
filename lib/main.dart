4fb0fcacbe754679b8b44b29a1c867a725670a40
Kevin Brown <kevin@hrinnovate.org>
2025-06-14 10:09:14 +0300

    app color

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:quiz1/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp( MaterialApp(
+    home: Scaffold(
+    body: Container(
+    decoration: const BoxDecoration(
+      gradient: LinearGradient(
+        colors: [
+      Color.fromARGB(255, 53, 6, 61),
+      Color.fromARGB(255, 29, 10, 33), 
+    ]
+    ),
+    ),
+    child: const StartScreen(),
+    ),
+    ),
+    ),
+    );
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,7 +4,9 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context){
-    return const Text('wookie meat');
+    return const Center(
+      child: Text('wookie meat'),
+    );
   }
 
 }
